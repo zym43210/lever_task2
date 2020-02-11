ad267a353e8c37373face1de56ab016c41bdb655
zym43210 <zymzom@yandex.ru>
2020-02-12 00:52:58 +0300

    used total ordering and made __eq__

--- main.py
+++ main.py
@@ -1,26 +1,32 @@
+from functools import total_ordering
+
+@total_ordering
 class Version:
+
+    
+    
     def __init__(self, version):
         self.version=version.split(".")
-    def length_checker(selfobj,otherobj):
-        if len(selfobj)>=len(otherobj):
-            return True
-        else :
-            return False
-    def __lt__(self,other):
 
-        if(Version.length_checker(self.version,other.version)) :
-            pass
-        else:
-            self.version,other.version=other.version,self.version
-        counter_of_secondVer=0;
-        for steps in self.version:
+    
+  
+    def __eq__(self, other):
+        if len(self.version)==len(other.version):
+            result_of_check=0
+            for first_version, second_version in zip(self.version, other.version):
 
-            if steps<other.version[counter_of_secondVer]:
+                if first_version==second_version:
+                    result_of_check+=1
+                else: 
+                    result_of_check=0
+            if result_of_check==len(self.version):
                 return True
-            elif steps>other.version[counter_of_secondVer]:
+            else:
                 return False
-            counter_of_secondVer=counter_of_secondVer+1
-            
+        
+    
+
+   
 
        
 
@@ -30,14 +36,16 @@ def main():
         ('1.0.0', '1.42.0'),
         ('1.2.0', '1.2.42'),
         ('1.1.0-alpha', '1.2.0-alpha.1'),
-        ('1.0.1b', '1.0.10-alpha.beta'),
-        ('1.0.0-rc.1', '1.0.0'),
+        ('1.0.1-beta', '1.0.10-alpha.beta'),
+        ('1.0.0','1.0.0-rc.1'),
     ]
 
     for version_1, version_2 in to_test:
-        print("kek")
-        assert Version(version_1) < Version(version_2), 'le failed'
-    
+        print(version_1)
+        print(version_2)
+        
+        assert Version(version_1) < Version(version_2), 'lt failed'
+        assert Version(version_2) != Version(version_1), 'neq failed'
 
 
 if __name__ == "__main__":
