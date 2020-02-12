bfe4a6fcb390d26754936728009cfe496050f877
zym43210 <zymzom@yandex.ru>
2020-02-13 01:33:11 +0300

    rewrite to PEP8, renamed some Var and methods

--- main.py
+++ main.py
@@ -1,54 +1,44 @@
 from functools import total_ordering
 import str_worker
 import common_part
+
+
 @total_ordering
 class Version:
 
-    
-    
     def __init__(self, version):
-        self.version=version.split(".")
+        self.version = version.split(".")
 
-    
-  
     def __eq__(self, other):
-        if len(self.version)==len(other.version):
-            result_of_check=0
+        if len(self.version) == len(other.version):
+            result_of_check = 0
             for first_version, second_version in zip(self.version, other.version):
 
-                if first_version==second_version:
-                    result_of_check+=1
-                else: 
-                    result_of_check=0
-            if result_of_check==len(self.version):
+                if first_version == second_version:
+                    result_of_check += 1
+                else:
+                    result_of_check = 0
+            if result_of_check == len(self.version):
                 return True
             else:
                 return False
-        
-    def __lt__(self,other):
-        
-        
-        extra_splitted_list1=str_worker.extra_split(self.version)
-        extra_splitted_list2=str_worker.extra_split(other.version)
-        ready_for_operations_list1=str_worker.to_numbers_format(extra_splitted_list1)
-        ready_for_operations_list2=str_worker.to_numbers_format(extra_splitted_list2)
-        
-       
-        if len(ready_for_operations_list1)==len( ready_for_operations_list2) and common_part.common_part_less(ready_for_operations_list1, ready_for_operations_list2):
-        
+
+    def __lt__(self, other):
+
+        extra_splitted_list1 = str_worker.split_dash(self.version)
+        extra_splitted_list2 = str_worker.split_dash(other.version)
+        ready_for_operations_list1 = str_worker.to_numbers_format(
+            extra_splitted_list1)
+        ready_for_operations_list2 = str_worker.to_numbers_format(
+            extra_splitted_list2)
+
+        if len(ready_for_operations_list1) == len(ready_for_operations_list2) and common_part.common_part_less(ready_for_operations_list1, ready_for_operations_list2):
+
             return True
-        elif len(ready_for_operations_list1)<len(ready_for_operations_list2) and common_part.common_part_equal( ready_for_operations_list1, ready_for_operations_list2):
-            
+        elif len(ready_for_operations_list1) < len(ready_for_operations_list2) and common_part.common_part_equal(ready_for_operations_list1, ready_for_operations_list2):
+
             return True
 
-        
-   
-   
-    
-    
-   
-
-       
 
 def main():
     to_test = [
@@ -57,12 +47,11 @@ def main():
         ('1.2.0', '1.2.42'),
         ('1.1.0-alpha', '1.2.0-alpha.1'),
         ('1.0.1-beta', '1.0.10-alpha.beta'),
-        ('1.0.0','1.0.0-rc.1'),
+        ('1.0.0', '1.0.0-rc.1'),
     ]
 
     for version_1, version_2 in to_test:
-        
-        
+
         assert Version(version_1) < Version(version_2), 'lt failed'
         assert Version(version_2) > Version(version_1), 'gt failed'
         assert Version(version_2) != Version(version_1), 'neq failed'
--- str_worker.py
+++ str_worker.py
@@ -1,25 +1,26 @@
 
-def extra_split(version):
-    splitted_list=[]
+def split_dash(version):
+    splitted_list = []
     for vers_step in version:
-            
-	    for split_step in vers_step.split("-"):
-	    	splitted_list.append(split_step)
 
-    return splitted_list       
+        for split_step in vers_step.split("-"):
+            splitted_list.append(split_step)
+
+    return splitted_list
+
+
 def to_numbers_format(my_list):
-    prioriti_dict={
-    	"alpha":0,
-        "beta":1,
-        "rc":2
+    prioriti_dict = {
+        "alpha": 0,
+        "beta": 1,
+        "rc": 2
     }
 
-    to_numbers_list=[]
+    to_numbers_list = []
 
     for list_step in my_list:
-        if prioriti_dict.get(list_step)!=None:
+        if prioriti_dict.get(list_step) != None:
             to_numbers_list.append(str(prioriti_dict.get(list_step)))
         else:
             to_numbers_list.append(list_step)
     return to_numbers_list
-		
